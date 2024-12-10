32c14eec8ca8772df8d992aa8ebc78599bc62960
Nathan Garzya Santoso <85006633+canonflow@users.noreply.github.com>
2024-12-10 20:06:51 +0700

    feat(redis): Add redis client

---
+++ src/lib/db.js
@@ -0,0 +1,17 @@
+import { createClient } from 'redis';
+
+const client = createClient({
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT
+    }
+});
+
+client.on('error', err => console.log('Redis Client Error', err));
+if (!client.isOpen) {
+    client.connect()
+}
+
+export { client }
